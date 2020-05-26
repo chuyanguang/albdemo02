19620b916a1cf3ce7b3c8ddeda98ef2ca6011737
Administrator <393160250@qq.com>
2020-05-26 16:37:02 +0800

    nio test

--- albdemo01/src/test/java/com/example/demo/DemoApplicationTests.java
+++ albdemo01/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+//@SpringBootTest
 class DemoApplicationTests {
 
     @Test
-    public void test01(){
-
+    public void test01() {
+        try (FileChannel fc = FileChannel.open(Paths.get("D:/test.txt"), StandardOpenOption.READ);) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            fc.read(buffer);
+            buffer.flip();
+            byte[] bytes = new byte[1024];
+            while (buffer.hasRemaining()) {
+                buffer.get(bytes, 0, buffer.limit());
+                System.out.println(new String(bytes, Charset.forName("UTF-8")));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
