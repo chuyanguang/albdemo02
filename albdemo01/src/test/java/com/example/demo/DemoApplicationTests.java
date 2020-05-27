cbd9c8e1b9b4e8674fcd6747ea89cbd7301284f8
Administrator <393160250@qq.com>
2020-05-27 16:49:58 +0800

    nio test

--- albdemo01/src/test/java/com/example/demo/DemoApplicationTests.java
+++ albdemo01/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -15,16 +15,41 @@ class DemoApplicationTests {
     public void test01() {
         try (FileChannel fc = FileChannel.open(Paths.get("D:/test.txt"), StandardOpenOption.READ);) {
             ByteBuffer buffer = ByteBuffer.allocate(1024);
-            fc.read(buffer);
-            buffer.flip();
             byte[] bytes = new byte[1024];
-            while (buffer.hasRemaining()) {
+            while (fc.read(buffer) != -1){
+                buffer.flip();
                 buffer.get(bytes, 0, buffer.limit());
                 System.out.println(new String(bytes, Charset.forName("UTF-8")));
+                buffer.clear();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+    }
+
+    @Test
+    public void test02() {
+        try (
+                FileChannel fc = FileChannel.open(Paths.get("D:/test.txt"), StandardOpenOption.READ);
+                FileChannel fc2 = FileChannel.open(Paths.get("D:/abc.txt"), StandardOpenOption.CREATE, StandardOpenOption.WRITE)
+        ) {
+            /*第一种方式*/
+//            ByteBuffer buffer = ByteBuffer.allocate(1024);
+//            while(fc.read(buffer) != -1){
+//                buffer.flip();
+//                fc2.write(buffer);
+//                buffer.clear();
+//            }
+            /*第二种方式*/
+//            fc.transferTo(0, fc.size(), fc2);
+            // 或者
+            fc2.transferFrom(fc, 0, fc.size());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 
 }
