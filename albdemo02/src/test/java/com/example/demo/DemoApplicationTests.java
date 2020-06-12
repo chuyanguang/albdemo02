d6cafa01cfd7a79b6b8e6ba39f96a1362689fd1f
Administrator <393160250@qq.com>
2020-06-12 17:02:07 +0800

    整合redis

--- albdemo02/src/test/java/com/example/demo/DemoApplicationTests.java
+++ albdemo02/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import com.alibaba.fastjson.JSONObject;
-import com.example.demo.entity.dto.DeptDo;
-import com.example.demo.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +10,32 @@ import org.springframework.data.redis.core.RedisTemplate;
 @SpringBootTest
 class DemoApplicationTests {
 
+    //    @Autowired
+//    private RedisUtil redisUtil;
     @Autowired
-    private RedisUtil redisUtil;
+    private RedisTemplate<String, String> redisTemplate;
 
     @Test
-    void contextLoads() {
-        String key1 = "key0003";
-        boolean hasKey = redisUtil.hasKey(key1);
-        log.info("{}:{}", key1 ,hasKey);
-        if (hasKey){
-            Object val = redisUtil.get(key1);
+    public void test01() {
+        String key1 = "key0001";
+        Boolean hasKey = redisTemplate.hasKey(key1);
+        log.info("{}:{}", key1, hasKey);
+        if (hasKey) {
+            String val = redisTemplate.opsForValue().get(key1);
             log.info("{}:{}", key1, val);
         }
     }
+
+//    @Test
+//    void contextLoads() {
+//        String key1 = "key0003";
+//        boolean hasKey = redisUtil.hasKey(key1);
+//        log.info("{}:{}", key1 ,hasKey);
+//        if (hasKey){
+//            Object val = redisUtil.get(key1);
+//            log.info("{}:{}", key1, val);
+//        }
+//    }
+
 
 }
