4030234a2f97563f3ad231965f1b2c2fd76e8ccf
Administrator <393160250@qq.com>
2020-06-23 14:56:44 +0800

    mongodb test

--- demo_mongodb/src/main/java/com/example/demo/entity/UserDo.java
+++ demo_mongodb/src/main/java/com/example/demo/entity/UserDo.java
@@ -3,20 +3,32 @@ package com.example.demo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "test01")
 public class UserDo implements Serializable {
 
     private static final long serialVersionUID = 1181351625678008748L;
 
-    private Long id;
+    @Id
+    @Field(name = "_id")
+    private Long _id;
 
+    @Field(name = "name")
     private String name;
 
+    @Field(name = "age")
     private Integer age;
 
+    public UserDo(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
 }
--- demo_mongodb/src/test/java/com/example/demo/DemoMongodbApplicationTests.java
+++ demo_mongodb/src/test/java/com/example/demo/DemoMongodbApplicationTests.java
@@ -1,34 +1,91 @@
 package com.example.demo;
 
+import cn.hutool.core.util.RandomUtil;
 import com.example.demo.entity.UserDo;
 import com.example.demo.utils.MongoUtil;
-import com.mongodb.client.MongoCollection;
+import com.google.common.collect.Lists;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
 
 @Slf4j
 @SpringBootTest
 class DemoMongodbApplicationTests {
 
+    private final static String COLLECTION_NAME = "test01";
+
     @Autowired
     private MongoTemplate mongoTemplate;
 
     @Test
-    void test01() {
-        String collectionName = "test01";
-        UserDo user = new UserDo();
-        user.setName("张华");
-        user.setAge(17);
+    void testInsert() {
+        List<Document> list = Lists.newArrayList();
+        for (int i = 0; i < 100; i++) {
+            UserDo user1 = new UserDo((long) i, "test" + i, RandomUtil.randomInt(1, 100));
+            list.add(MongoUtil.objToDoc(user1));
+        }
+        Collection<Document> collection = mongoTemplate.insert(list, UserDo.class);
+//        InsertManyResult result = mongoTemplate.getCollection(COLLECTION_NAME).insertMany(list);
+//        if (result.wasAcknowledged()) {
+//            MongoCollection<Document> collection = mongoTemplate.getCollection(COLLECTION_NAME);
+//            for (Document doc : collection.find()) {
+//                UserDo userDo = MongoUtil.docToObj(doc, UserDo.class);
+//                log.info(userDo.toString());
+//            }
+//        }
+    }
 
-        mongoTemplate.getCollection(collectionName).insertOne(MongoUtil.objToDoc(user));
-        MongoCollection<Document> collection = mongoTemplate.getCollection(collectionName);
-        for (Document doc : collection.find()) {
-            UserDo userDo = MongoUtil.docToObj(doc, UserDo.class);
-            log.info(userDo.getName());
+    @Test
+    void testQuery() {
+        List<Sort.Order> orderList = Lists.newArrayList();
+        orderList.add(new Sort.Order(Sort.Direction.DESC, "age"));
+
+        Query query = new Query().skip(10).limit(10).with(Sort.by(Sort.Order.desc("age"), Sort.Order.desc("_id")));
+        Pattern pattern = Pattern.compile("^.*test.*$", Pattern.CASE_INSENSITIVE);
+        query.addCriteria(Criteria.where("name").regex(pattern));
+
+        List<UserDo> userList = mongoTemplate.find(query, UserDo.class, COLLECTION_NAME);
+        for (UserDo user : userList) {
+            log.info(user.toString());
+        }
+    }
+
+    @Test
+    void testUpdate() {
+//        Query query = new Query().addCriteria(Criteria.where("name").is("张三"));
+//        Update update = new Update().inc("age", 2);
+//        UpdateResult updateResult = mongoTemplate.updateFirst(query, update, COLLECTION_NAME);
+//        if (updateResult.wasAcknowledged()){
+//            log.info("update success!");
+//        }
+
+        Query query = new Query().addCriteria(Criteria.where("name").is("test7"));
+        Update update = new Update().set("age", 21);
+        UpdateResult updateResult = mongoTemplate.updateFirst(query, update, COLLECTION_NAME);
+        if (updateResult.wasAcknowledged()) {
+            log.info("update success!");
+        }
+    }
+
+    @Test
+    void testDel() {
+        Query query = new Query().addCriteria(Criteria.where("name").is("张华"));
+        DeleteResult deleteResult = mongoTemplate.remove(query, COLLECTION_NAME);
+        if (deleteResult.wasAcknowledged()) {
+            log.info("delete success!");
         }
     }
 
